97b5385371b98fc4718277aefe87127fcbe1dae5
Florian Weber <florian.weber@ueberbit.de>
2016-11-29 15:16:19 +0100

    Fix Deprecation notice: Resource references should now be capitalized

--- manifests/fpm.pp
+++ manifests/fpm.pp
@@ -26,7 +26,7 @@ define php2::fpm (
   # Set up FPM config
   file { $fpm_config:
     content => template('php2/php-fpm.conf.erb'),
-    notify  => ::php2::fpm::service[$version],
+    notify  => Php2::fpm::service[$version],
   }
 
   # Set up FPM Pool config directory
